ad61dda31e5aadf6fc91fb0fc0b2819fe5d4de73
alejandro.delio <alejandro.delio@5dd192be-677e-11de-b8ef-473237edd27e>
2009-10-06 02:26:42 +0000

    fix

---
+++ trunk/citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/bienvenidaCliente/MostrarViajesAnteriores.java
@@ -0,0 +1,15 @@
+package ar.edu.utn.frba.proyecto.citysoft.controller.bienvenidaCliente;
+
+import org.zkoss.zul.Listbox;
+
+// TODO: Che gabriel, toque un par de cosas de esta clase:
+//1- Le saquè el "public" que antecedia a a palabra "class"
+//2- corregi "extendes" por "extends"
+//3- Le puse un nombre de clase igual al nombre de archivo (asi requiere java)
+//4- Puse la primer letra ("M") en mayuscula. Por convencion, el nomrbe de clases va en may
+class MostrarViajesAnteriores extends Listbox {
+
+	public void listarTodos() {
+	}
+
+}
