0ae2da217041260552a6573177fc647927688fdb
alejandro.delio <alejandro.delio@5dd192be-677e-11de-b8ef-473237edd27e>
2009-10-07 03:27:42 +0000

    ABM de clientes total y completo

---
+++ trunk/citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/ImagenConIdDeNegocio.java
@@ -0,0 +1,34 @@
+package ar.edu.utn.frba.proyecto.citysoft.controller;
+
+import org.zkoss.zul.Image;
+
+/**
+ * Es un elemento <image> comun y corriente para paginas ZUL, solo que permite
+ * usar el atributo "idNegocio" como si fuera un custom-attribute.
+ * 
+ * La razon por la que usamos esto en lugar de un custom-attribute, es que al
+ * parecer los custom-attributes no soportan tomar valor desde variables
+ * originadas en el databinder.
+ * 
+ * @see listadoCliente.zul
+ * @author Alejandro
+ */
+public class ImagenConIdDeNegocio extends Image {
+
+	private static final long serialVersionUID = 3163353167068288629L;
+
+	// **************************************
+	// ** EJECUCION
+	// **************************************
+
+	public int idNegocio;
+
+	public int getIdNegocio() {
+		return this.idNegocio;
+	}
+
+	public void setIdNegocio(int idNegocio) {
+		this.idNegocio = idNegocio;
+	}
+
+}
