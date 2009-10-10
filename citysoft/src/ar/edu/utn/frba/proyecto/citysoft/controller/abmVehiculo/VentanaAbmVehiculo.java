57c7b3ccddb5a39b84b1ca0a5e7572e43f87b630
galan.hernan <galan.hernan@5dd192be-677e-11de-b8ef-473237edd27e>
2009-10-10 08:45:24 +0000

    alta vehiculos

--- citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/abmVehiculo/VentanaAbmVehiculo.java
+++ citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/abmVehiculo/VentanaAbmVehiculo.java
@@ -1,7 +1,9 @@
 package ar.edu.utn.frba.proyecto.citysoft.controller.abmVehiculo;
 
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Button;
 import org.zkoss.zul.Window;
-import org.zkoss.zul.api.Combobox;
 import org.zkoss.zul.api.Intbox;
 import org.zkoss.zul.api.Textbox;
 
@@ -10,34 +12,212 @@ import ar.edu.utn.frba.proyecto.citysoft.modelo.Taxi;
 
 public class VentanaAbmVehiculo extends Window {
 
-	public void altaVehiculo() {
-		Textbox txtNombres = (Textbox) this.getFellow("txtNombres");
-		Textbox txtApellido = (Textbox) this.getFellow("txtApellido");
+	private static final long serialVersionUID = 8473212146219211650L;
 
-		Combobox txtTipoDoc = (Combobox) this.getFellow("cmbTipoDoc");
-		Intbox intNroDoc = (Intbox) this.getFellow("intNroDoc");
+	// **************************************
+	// ** ELEMENTOS
+	// **************************************
 
-		Textbox txtPatente = (Textbox) this.getFellow("txtPatente");
-		Textbox txtMarca = (Textbox) this.getFellow("txtMarca");
-		Textbox txtModelo = (Textbox) this.getFellow("txtModelo");
-		Textbox txtPoliza = (Textbox) this.getFellow("txtPoliza");
-		
-		Combobox txtTipoVeh = (Combobox) this.getFellow("cmbTipoVeh");
+	private Textbox elemPoliza() {
+		return (Textbox) this.getFellow("txtPoliza");
+	}
 
-		// **************************************
-		// ** Creo el vehiculo!!!
-		// **************************************
+	private Textbox elemMarca() {
+		return (Textbox) this.getFellow("txtMarca");
+	}
 
-		Taxi t = new Taxi();
-		t.setIdTaxi(888);
-		t.setMarca(txtMarca.getValue());
-		t.setModelo(txtModelo.getValue());
-		
-		// **************************************
-		// ** Guardo el taxi en la central!!!!
-		// **************************************
+	private Textbox elemModelo() {
+		return (Textbox) this.getFellow("txtModelo");
+	}
 
-		CentralTaxis.getInstance().addTaxi(t);
+	private Intbox elemNroDocumento() {
+		return (Intbox) this.getFellow("intNroDoc");
+	}
+
+	private Textbox elemApellido() {
+		return (Textbox) this.getFellow("txtApellido");
+	}
+
+	private Textbox elemNombres() {
+		return (Textbox) this.getFellow("txtNombres");
+	}
+
+	private Intbox elemPatente() {
+		return (Intbox) this.getFellow("intPatente");
+	}
+
+	// **************************************
+	// ** ELEMENTOS - BOTONES
+	// **************************************
+
+	private Button elemModificar() {
+		return (Button) this.getFellow("btnModificar");
+	}
+
+	private Button elemEliminar() {
+		return (Button) this.getFellow("btnEliminar");
+	}
+
+	private Button elemCerrar() {
+		return (Button) this.getFellow("btnCerrar");
+	}
+
+	private Button elemGuardarAlta() {
+		return (Button) this.getFellow("btnGuardarAlta");
+	}
+
+	private Button elemGuardarModif() {
+		return (Button) this.getFellow("btnGuardarModif");
+	}
+
+	private Button elemCancelar() {
+		return (Button) this.getFellow("btnCancelar");
+	}
+
+	// **************************************
+	// ** Estados de la ventana
+	// **************************************
+
+	public void abrirAlta() {
+		limpiarCampos();
+		elemModificar().setVisible(false);
+		elemEliminar().setVisible(false);
+		elemCerrar().setVisible(false);
+		elemGuardarAlta().setVisible(true);
+		elemGuardarModif().setVisible(false);
+		elemCancelar().setVisible(true);
+	}
+
+	public void abrirDetalle(int idVehiculo) {
+		// Primero deshabilitamos los campos
+		elemPatente().setReadonly(true);
+		elemNombres().setReadonly(true);
+		elemApellido().setReadonly(true);
+		elemNroDocumento().setReadonly(true);
+		elemPoliza().setReadonly(true);
+		elemMarca().setReadonly(true);
+		elemModelo().setReadonly(true);
+		// Luego, traemos los valores
+		traerDetalleVehiculo(idVehiculo);
+		// Despues, habilitamos y deshabilitamos los botones
+		elemModificar().setVisible(true);
+		elemEliminar().setVisible(true);
+		elemCerrar().setVisible(true);
+		elemGuardarAlta().setVisible(false);
+		elemGuardarModif().setVisible(false);
+		elemCancelar().setVisible(false);
+		// Finalmente, abrimos la ventana
+		this.setVisible(true);
+	}
+
+	public void abrirModificacion() {
+		// Habilitamos los campos pertientes
+		elemPatente().setReadonly(true);
+		elemNombres().setReadonly(false);
+		elemApellido().setReadonly(false);
+		elemNroDocumento().setReadonly(false);
+		elemPoliza().setReadonly(false);
+		elemMarca().setReadonly(false);
+		elemModelo().setReadonly(false);
+		// Despues, habilitamos y deshabilitamos los botones
+		elemModificar().setVisible(false);
+		elemEliminar().setVisible(false);
+		elemCerrar().setVisible(false);
+		elemGuardarAlta().setVisible(false);
+		elemGuardarModif().setVisible(true);
+		elemCancelar().setVisible(true);
+		// Finalmente, abrimos la ventana
+		this.setVisible(true);
+	}
+
+	public void abrirModificacion(int idVehiculo) {
+		traerDetalleVehiculo(idVehiculo);
+		this.abrirModificacion();
+	}
+
+	/**
+	 * Si nos abren desde otra ventana, la otra ventana se puede colgar del
+	 * evento ON_CLOSE de esta ventana para hacer cosas porque para cerrar
+	 * posteamos un ON_CLOSE. Si fuesemos a cerrar la ventana llamando a
+	 * detach(), el evento ON_CLOSE no se postea y la ventana padre no tiene
+	 * forma de enterarse si esta se cierra o no
+	 */
+	public void cerrar() {
+		Events.postEvent(new Event(Events.ON_CLOSE, this));
+	}
+
+	// **************************************
+	// ** Ejecucion
+	// **************************************
+
+	public void altaTaxi() {
+		// Creo el taxi!!!
+		Taxi c = new Taxi();
+		c.setIdTaxi(elemPatente().getValue());
+		c.setApellido(elemApellido().getValue());
+		c.setNombre(elemNombres().getValue());
+		// c.setTipoDocumento(elemTipoDocumento().getValue());
+		c.setDni(elemNroDocumento().getValue());
+		c.setPoliza(elemPoliza().getValue());
+		c.setMarca(elemMarca().getValue());
+		c.setModelo(elemModelo().getValue());
+
+		// Guardo el cliente en la central!!!!
+		CentralTaxis.getInstance().addTaxi(c);
+
+		// Cierro la ventana
+		this.cerrar();
+	}
+
+	public void modifTaxi() {
+		// Creo el taxi!!!
+		Taxi c = CentralTaxis.getInstance().getTaxiPorId(elemPatente().getValue());
+
+		c.setApellido(elemApellido().getValue());
+		c.setNombre(elemNombres().getValue());
+		// c.setTipoDocumento(elemTipoDocumento().getValue());
+		c.setDni(elemNroDocumento().getValue());
+		c.setPoliza(elemPoliza().getValue());
+		c.setMarca(elemMarca().getValue());
+		c.setModelo(elemModelo().getValue());
+
+		// Actualizo el cliente en la central!!!!
+		CentralTaxis.getInstance().addTaxi(c);
+
+		// Cierro la ventana
+		this.cerrar();
+	}
+
+	public void eliminarTaxi() {
+		int idVehiculo = elemPatente().intValue();
+		Taxi c = CentralTaxis.getInstance().getTaxiPorId(idVehiculo);
+		CentralTaxis.getInstance().getTaxis().remove(c);
+		this.cerrar();
+	}
+
+	// **************************************
+	// ** HELPERS
+	// **************************************
+
+	private void traerDetalleVehiculo(int idVehiculo) {
+		Taxi c = CentralTaxis.getInstance().getTaxiPorId(idVehiculo);
+		elemPatente().setValue(idVehiculo);
+		elemNombres().setValue(c.getNombre());
+		elemApellido().setValue(c.getApellido());
+		elemNroDocumento().setValue(c.getDni());
+		elemPoliza().setValue(c.getPoliza());
+		elemMarca().setValue(c.getMarca());
+		elemModelo().setValue(c.getModelo());
+	}
+
+	private void limpiarCampos() {
+		elemPatente().setValue(null);
+		elemNombres().setValue(null);
+		elemApellido().setValue(null);
+		elemNroDocumento().setValue(null);
+		elemPoliza().setValue(null);
+		elemMarca().setValue(null);
+		elemModelo().setValue(null);
 	}
 
 }
---
+++ citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/listadoVehiculos/VentanaListadoVehiculos.java
@@ -0,0 +1,66 @@
+package ar.edu.utn.frba.proyecto.citysoft.controller.listadoVehiculos;
+
+import java.util.Collection;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Window;
+
+import ar.edu.utn.frba.proyecto.citysoft.controller.abmVehiculo.VentanaAbmVehiculo;
+import ar.edu.utn.frba.proyecto.citysoft.modelo.CentralTaxis;
+import ar.edu.utn.frba.proyecto.citysoft.modelo.Taxi;
+
+public class VentanaListadoVehiculos extends Window {
+
+	private static final long serialVersionUID = 9103912640176445638L;
+
+	// **************************************
+	// ** EJECUCION
+	// **************************************
+
+	public Collection<Taxi> getListaVehiculos() {
+		SortedSet<Taxi> lista = new TreeSet<Taxi>(CentralTaxis.getInstance().getTaxis());
+		return lista;
+	}
+
+	public void eliminar(int patente) {
+		Taxi c = CentralTaxis.getInstance().getTaxiPorId(patente);
+		CentralTaxis.getInstance().getTaxis().remove(c);
+		this.refrescarTabla();
+	}
+
+	public void modificar(int patente) {
+		Component componenteAbmVehiculo = Executions.createComponents("altaVehiculo.zul", null, null);
+		VentanaAbmVehiculo win = (VentanaAbmVehiculo) componenteAbmVehiculo.getFellow("winAltaVehiculo");
+		win.addEventListener(Events.ON_CLOSE, new OnCloseRefrescarTabla(this));
+		win.abrirModificacion(patente);
+	}
+
+	public void refrescarTabla() {
+		Event event = new Event(Events.ON_CHANGE, this.getFellow("grid"));
+		Events.sendEvent(event);
+	}
+
+	// **************************************
+	// ** INNER CLASS. ¿¿¿Servirá para extenderla hacia afuera???
+	// **************************************
+
+	private class OnCloseRefrescarTabla implements EventListener {
+		private VentanaListadoVehiculos win;
+
+		public OnCloseRefrescarTabla(VentanaListadoVehiculos win) {
+			this.win = win;
+		}
+
+		@Override
+		public void onEvent(Event event) throws Exception {
+			this.win.refrescarTabla();
+		}
+	}
+
+}
