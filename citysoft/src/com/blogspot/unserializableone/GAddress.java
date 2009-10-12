34ab575ae02984251d3f063ea8935be26a315370
alejandro.delio <alejandro.delio@5dd192be-677e-11de-b8ef-473237edd27e>
2009-10-12 21:30:33 +0000

    geocodificador

---
+++ citysoft/src/com/blogspot/unserializableone/GAddress.java
@@ -0,0 +1,75 @@
+package com.blogspot.unserializableone;
+
+public class GAddress {
+
+	public String address;
+	public String fullAddress;
+	public String zipCode;
+	public String city;
+	public String state;
+	public double lat;
+	public double lng;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFullAddress() {
+		return fullAddress;
+	}
+
+	public void setFullAddress(String fullAddress) {
+		this.fullAddress = fullAddress;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+
+	public String toString() {
+		return "Direccion: [" + this.address + "], Direccion completa: [" + this.fullAddress
+				+ "], Codigo postal: [" + this.fullAddress + "], Ciudad: [" + this.city
+				+ "], Estado: [" + this.state + "] (lat: " + this.lat + "; lon: " + this.lng + ").";
+	}
+
+}
