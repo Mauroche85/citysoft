e91207b3f79cda58e8c4d3bfdfd19a917a63a90a
alejandro.delio <alejandro.delio@5dd192be-677e-11de-b8ef-473237edd27e>
2009-11-04 00:43:39 +0000

    constantes en nombres de archivos

--- citysoft/src/ar/edu/utn/frba/proyecto/citysoft/utils/WebUtils.java
+++ citysoft/src/ar/edu/utn/frba/proyecto/citysoft/utils/WebUtils.java
@@ -3,6 +3,8 @@ package ar.edu.utn.frba.proyecto.citysoft.utils;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 
+import org.zkoss.zk.ui.Executions;
+
 public class WebUtils {
 
 	public static String getContextPath(ServletRequest request) {
@@ -13,6 +15,20 @@ public class WebUtils {
 	public static String getRequestUri(ServletRequest request) {
 		HttpServletRequest req = (HttpServletRequest) request;
 		return req.getRequestURI();
+	}
+
+	/**
+	 * Devuelve la URI (como URL pero sin http://www.sigaEseTaxi.com.ar) para el
+	 * recurso recibido por parametro.
+	 * 
+	 * @param resource
+	 *            es la URI del recurso relativa a la aplicacion. Esto quiere
+	 *            decir que no tiene el contextPath. Ejemplo:
+	 *            getUri("/secure/follower") = "/citysoft/secure/follower"
+	 */
+	public static String getUri(String resource) {
+		HttpServletRequest req = (HttpServletRequest) Executions.getCurrent().getNativeRequest();
+		return WebUtils.getContextPath(req) + resource;
 	}
 
 }
