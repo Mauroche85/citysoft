667d1062af03a3fd1848e96eb5c5f2bfa0299729
alejandro.delio <alejandro.delio@5dd192be-677e-11de-b8ef-473237edd27e>
2009-11-11 03:30:26 +0000

    usuario operador

--- trunk/citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/cliente/ManipularComboUsuario.java
+++ trunk/citysoft/src/ar/edu/utn/frba/proyecto/citysoft/controller/cliente/ManipularComboUsuario.java
@@ -1,11 +1,11 @@
 package ar.edu.utn.frba.proyecto.citysoft.controller.cliente;
 
-import org.zkoss.zul.Window;
+import org.zkoss.zul.Combobox;
 
 import ar.edu.utn.frba.proyecto.citysoft.controller.ConstantesGeneralesDeVentanas;
 
-public class ManipularComboUsuario extends Window implements ConstantesGeneralesDeVentanas{
-	public void cargarUsuario(){
-	System.out.println("Cargar Usuario"); 
+public class ManipularComboUsuario extends Combobox implements ConstantesGeneralesDeVentanas {
+	public void cargarUsuario() {
+		System.out.println("Cargar Usuario");
 	}
 }
